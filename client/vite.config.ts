dc2b7bd03da4ad01649bb1f3cfbe1a1d1ce00e0e
J Kleuters <jkleuters@broadsight.nl>
2025-09-05 12:51:11 +0200

    voorkom jekyll builds

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -13,7 +13,8 @@ export default defineConfig({
   build: {
     outDir: 'docs', // ✅ use docs instead of dist
   },
-  base: './', // belangrijk voor relatieve paden
+  // base: './', // belangrijk voor relatieve paden
+  base: "/uitgeleend/", // ✅ set base path for GitHub Pages
   
 })
 
